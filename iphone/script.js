2b88f94eea409c7bcbbf28ff2fae8489757c7781
savinn <savinn85@gmail.com>
2019-12-06 23:20:47 +0100

    cleaning unused files

--- iphone/script.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const filmAbout = {
-  name: "once apon a time in america",
-  year: 1984,
-  about: "jew mafia in 20th 30th century",
-  actors: ["robert de niro", "jamse wood", "elizabeth mchower"]
-};
