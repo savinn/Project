bacf635ecf6c52b4a31164bed5fa4024c71505bd
goodman <abegday@gmail.com>
2019-12-06 19:30:58 +0200

    js films1

---
+++ iphone/JS/script.js
@@ -0,0 +1,7 @@
+"use strict";
+const filmAbout = {
+  name: "once apon a time in america",
+  year: "1984",
+  filmAbout: "jew mafia in 20th 30th century",
+  actor: ["robert de niro", "jamse wood", "elizabeth mchower"]
+};
